697537747368f0d18e7aadcd437c4e4743be3a9d
Kevin Einstein <kaitokai@live.com>
2014-05-27 13:20:14 -0700

    Added code to accept incoming JSON requests to the controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  def verified_request?
+    if request.content_type == "application/json"
+      true
+    else
+      super()
+    end
+  end
+  
   include SessionsHelper
   include ReservationsHelper
 end
