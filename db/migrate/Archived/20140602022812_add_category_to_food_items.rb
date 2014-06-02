a1ddea2603269400035751b45a8a08807a9d512b
= <=>
2014-06-02 13:12:54 -0700

    x

--- db/migrate/Archived/20140602022812_add_category_to_food_items.rb
+++ db/migrate/Archived/20140602022812_add_category_to_food_items.rb
@@ -1,12 +1,13 @@
 class AddCategoryToFoodItems < ActiveRecord::Migration
   def change
-  	create_table :food_items do |t|
-      t.string :name
-      t.float :price
-      t.string :description
-      t.string :category
-
-      t.timestamps
-  	end
+  	# create_table :food_items do |t|
+      # t.string :name
+      # t.float :price
+      # t.string :description
+      # t.string :category
+      # 
+      # t.timestamps
+  	# end
+  	add_column :food_items, :category, :string
   end
 end
