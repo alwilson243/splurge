c8ef0d4324066ec02e3d7d37b170b36fee67d922
= <=>
2014-06-02 13:47:47 -0700

    Schema fix?

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 20140602030041) do
     t.datetime "updated_at"
   end
 
+  create_table "menus", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "reservations", force: true do |t|
     t.integer  "restaurants_id"
     t.string   "name"
