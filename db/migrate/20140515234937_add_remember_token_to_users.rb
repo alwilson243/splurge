b5bfb3e1f1495db66c13cae6e46479cf015a67eb
= <=>
2014-06-02 19:23:46 -0700

    xx

--- db/migrate/20140515234937_add_remember_token_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddRememberTokenToUsers < ActiveRecord::Migration
-  def change
-    add_column :restaurants, :remember_token, :string
-    add_index  :restaurants, :remember_token
-  end
-end
