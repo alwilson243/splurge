2f83cf9d20e96d33424c84cd31aa229b44f383b4
= <=>
2014-06-02 17:58:20 -0700

    xx

--- db/migrate/Archived/20140529061817_add_dine_out_f_kand_rename_meal_time_type.rb
+++ db/migrate/Archived/20140529061817_add_dine_out_f_kand_rename_meal_time_type.rb
@@ -1,6 +1,6 @@
 class AddDineOutFKandRenameMealTimeType < ActiveRecord::Migration
   def change
-  	add_foreign_key "dine_outs", "restaurants", name: "dine_outs_restaurant_id_fk"
+  	# add_foreign_key "dine_outs", "restaurants", name: "dine_outs_restaurant_id_fk"
   	rename_column :dine_outs, :type, :kind
   	rename_column :reservations, :meal_time, :timeStart
   end
