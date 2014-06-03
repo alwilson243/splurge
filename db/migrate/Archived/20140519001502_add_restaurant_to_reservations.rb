1c63a12eb99e24cec87c5ed364d1768ca9700a79
= <=>
2014-06-02 17:33:43 -0700

    tt

--- db/migrate/Archived/20140519001502_add_restaurant_to_reservations.rb
+++
@@ -1,7 +0,0 @@
-class AddRestaurantToReservations < ActiveRecord::Migration
-  def change
-  	change_table :reservations do |t|
-      t.foreign_key :restaurants, dependent: :delete
-    end
-  end
-end
