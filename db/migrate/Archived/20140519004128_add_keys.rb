1cc16e68ad2a774aed5a2241906b7cea2db6b92d
= <=>
2014-06-02 17:54:20 -0700

    xx

--- db/migrate/Archived/20140519004128_add_keys.rb
+++ db/migrate/Archived/20140519004128_add_keys.rb
@@ -1,5 +1,5 @@
 class AddKeys < ActiveRecord::Migration
   def change
-    add_foreign_key "reservations", "restaurants", name: "reservations_restaurant_id_fk"
+    # add_foreign_key "reservations", "restaurants", name: "reservations_restaurant_id_fk"
   end
 end
